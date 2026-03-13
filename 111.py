894def4830d568f61cabfe7f7294b237a5a06af5
Yy <13540691368@163.com>
2026-03-13 16:39:54 +0800

    更新路径

---
+++ 111.py
@@ -0,0 +1,18 @@
+import random
+print(round(random.random(),2))
+print(random.randrange(1,200))
+print(len('hello'))
+print(bool("hello"))
+
+
+# for i in range(1,10):
+#     for j in range(1,10):
+#         if j<=i:
+#             print(str(j)+"*"+str(i),end=" ")
+#     print()
+#
+
+# x=2+3j
+# y=500
+# print(complex(y))
+
