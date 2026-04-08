483c59223fab3872306d75cd06dab8013b776c2b
Yy <13540691368@163.com>
2026-04-08 13:39:59 +0800

    20260407~9*9乘法表理解

--- 111.py
+++ 111.py
@@ -1,18 +1,22 @@
-import random
-print(round(random.random(),2))
-print(random.randrange(1,200))
-print(len('hello'))
-print(bool("hello"))
+# import random
+# print(round(random.random(),2))
+# print(random.randrange(1,200))
+# # print(len('hello'))
+# # print(bool("hello"))
 
 
-# for i in range(1,10):
-#     for j in range(1,10):
-#         if j<=i:
-#             print(str(j)+"*"+str(i),end=" ")
-#     print()
-#
+for i in range(1,10):
+    for j in range(1,10):
+        if j<=i:
+            print(str(j)+"*"+str(i),end=" ")
+    print()
 
-# x=2+3j
-# y=500
-# print(complex(y))
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
+for k in range(1,10):
+    for l in range(1,10):
+        if l>=k:
+            print(str(k)+"*"+str(l),end=" ")
+    print()
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
